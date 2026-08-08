2afe1e59839974ec6290cf88825eeb77e5e5419b
haileykyllonen <hkyllo1@wgu.edu>
2026-08-08 13:21:33 -0500

    Fixed flake8 issues

--- ml/data.py
+++ ml/data.py
@@ -3,47 +3,54 @@ from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
 
 
 def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
+    X,
+    categorical_features=None,
+    label=None,
+    training=True,
+    encoder=None,
+    lb=None,
 ):
-    """ Process the data used in the machine learning pipeline.
+    """Process the data used in the machine learning pipeline.
 
-    Processes the data using one hot encoding for the categorical features and a
-    label binarizer for the labels. This can be used in either training or
-    inference/validation.
+    Processes the data using one hot encoding for the categorical features
+    and a label binarizer for the labels. This can be used in either
+    training or inference/validation.
 
-    Note: depending on the type of model used, you may want to add in functionality that
-    scales the continuous data.
+    Note: depending on the type of model used, you may want to add
+    functionality that scales the continuous data.
 
     Inputs
     ------
     X : pd.DataFrame
         Dataframe containing the features and label.
         Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
+    categorical_features : list[str]
+        List containing the names of the categorical features
     label : str
-        Name of the label column in `X`. If None, then an empty array will
-        be returned for y (default=None)
+        Name of the label column in `X`. If None, then an empty array
+        will be returned for y
     training : bool
-        Indicator if training mode or inference/validation mode.
+        Indicator if training mode or inference/validation mode
     encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
+        Trained sklearn OneHotEncoder, only used if training=False
     lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
+        Trained sklearn LabelBinarizer, only used if training=False
 
     Returns
     -------
     X : np.array
-        Processed data.
+        Processed data
     y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
+        Processed labels if labeled=True, otherwise empty np.array
     encoder : sklearn.preprocessing._encoders.OneHotEncoder
         Trained OneHotEncoder if training is True, otherwise returns
-        the encoder passed in.
+        the encoder passed in
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained LabelBinarizer if training is True, otherwise returns
-        the binarizer passed in.
+        the binarizer passed in
     """
+    if categorical_features is None:
+        categorical_features = []
 
     if label is not None:
         y = X[label]
@@ -52,9 +59,9 @@ def process_data(
         y = np.array([])
 
     X_categorical = X[categorical_features].values
-    X_continuous = X.drop(*[categorical_features], axis=1)
+    X_continuous = X.drop(columns=categorical_features)
 
-    if training is True:
+    if training:
         encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
         lb = LabelBinarizer()
         X_categorical = encoder.fit_transform(X_categorical)
@@ -63,7 +70,6 @@ def process_data(
         X_categorical = encoder.transform(X_categorical)
         try:
             y = lb.transform(y.values).ravel()
-        # Catch the case where y is None because we're doing inference.
         except AttributeError:
             pass
 
@@ -72,8 +78,7 @@ def process_data(
 
 
 def apply_label(inference):
-    """ Converts the binary label in a single inference sample into string."""
+    """Convert the binary label in one inference sample into a string."""
     if inference[0] == 1:
         return ">50K"
-    elif inference[0] == 0:
-        return "<=50K"
+    return "<=50K"
